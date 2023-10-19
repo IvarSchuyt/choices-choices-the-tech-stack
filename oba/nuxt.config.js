4e695d316ef7ce4ce7a866c9e9dce63ebc031dca
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2023-10-19 13:58:28 +0200

    lukt het?

---
+++ oba/nuxt.config.js
@@ -0,0 +1,6 @@
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/sanity'],
+  sanity: {
+    projectId: 'l5iz1u04',
+  },
+})
---
+++ oba/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'l5iz1u04',
+    dataset: 'production'
+  }
+})
