c0b721c936215e52e6f73709f0b8effb0c39c8a9
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2023-10-19 20:53:44 +0200

    test

--- oba/schemas/pet.js
+++ oba/schemas/pet.js
@@ -7,6 +7,11 @@ export default {
         name: 'name',
         type: 'string',
         title: 'Name'
+      },
+      {
+        name: 'height',
+        type: 'string',
+        title: 'Height'
       }
     ]
   }
